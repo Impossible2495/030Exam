077b480138c36dfe0be3eb87cd35711edb3ada31
Ruslan Abramov <abramov-24@yandex.ru>
2024-04-25 05:34:56 +0300

    add async function

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-
 // Плавная прокрутка скролла начало
 
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
- 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
+  });
 });
 
 // Плавная прокрутка скролла конец
+
+
+// Асинхронный метод отправки формы
+
+async function sendForm(form) {
+  let response = await fetch("send.php", {
+    method: "POST",
+    body: new FormData(form),
+  });
+  let result = await response.text();
+  if (result == "ok") {
+    alert("Вы успешно отправили заявку");
+  } else if (result == "wrong") {
+    alert("Что-то пошло не так");
+  }
+}
